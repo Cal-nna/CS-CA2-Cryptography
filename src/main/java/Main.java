ddb7630dac3623bc998b5ef7b0e06ad162cea389
Cal <d00269431@student.dkit.ie>
2024-11-26 14:59:32 +0000

    Updated on 26/11/2024 at 14:59pm

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -50,3 +50,4 @@ public class Main {
     }
 }
 //TODO, fix the file reading so it can comprehend seperate lines but only for the encrypting does this problem persist
+//TODO, Destroy the program, find any weaknesses
